e338209c4b87f6f2f1bb8a2810ca168537c59fbf
cookies <55610198+BIuet@users.noreply.github.com>
2022-04-18 21:33:07 -0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
+document.getElementById("discordIcon").onclick = (e) => {
+    let tooltip =  document.getElementById("tooltip");
+    navigator.clipboard.writeText("cookies#1686");
+    tooltip.innerHTML = "Copied!";
+    setTimeout(() => tooltip.style.opacity = 1, 100);
+    setTimeout(() => {
+      tooltip.style.opacity = 0;
+      setTimeout(()=> tooltip.innerHTML = "cookies#1686", 500);
+    }, 1000);
+};
 
+document.getElementById("discordIcon").onmouseover = (e) => {
+    let tooltip =  document.getElementById("tooltip");
+    tooltip.style.opacity = 1;
+};
+
+document.getElementById("discordIcon").onmouseleave = (e) => {
+    let tooltip = document.getElementById("tooltip");
+    tooltip.style.opacity = 0;
+};
